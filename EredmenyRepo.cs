f63f2ed4549b10eecef5094b9ea4722f196f5b32
Gyalog Bence <gyalog.bence_2020c@bolyai-szakkozep.hu>
2024-04-18 09:09:03 +0200

    Add project files.

---
+++ EredmenyRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using EredmenyekCLI;
+
+namespace EredmenyekCLI
+{
+    static class EredmenyRepository
+    {
+        public static string Path { get; set; }
+        public static char Delimiter { get; set; } = ',';
+        public static bool SkipHeader { get; set; }
+
+        public static List<Eredmeny> FindAll()
+        {
+            StreamReader r = new StreamReader(Path, true);
+            if (SkipHeader) { r.ReadLine(); }
+
+            List<Eredmeny> eredmenyListOutput = new List<Eredmeny> { };
+
+            while (!r.EndOfStream)
+            {
+                eredmenyListOutput.Add(new Eredmeny(r.ReadLine(), Delimiter));
+            }
+            return eredmenyListOutput;
+        }
+    }
+}
